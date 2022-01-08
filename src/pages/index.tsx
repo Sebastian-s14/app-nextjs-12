21d3b21e1e1207fd34eeb9d82ebb72cbeb031dc9
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2022-01-07 21:35:52 -0500

    feat: first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='bg-blue-400'>
+      Hello with Next12
+    </div>
+  )
+}
+
+export default Home
